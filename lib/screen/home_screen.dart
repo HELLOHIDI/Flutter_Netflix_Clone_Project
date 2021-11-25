8a98a3d84b27594596c155d506361f9505ff5403
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-11-25 22:51:34 +0900

    home_Screen 복습 및 주석 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -53,8 +53,15 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      //왼쪽, 위쪽, 오른쪽, 아래쪽에 padding 생성
       padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      // 탑바 제작
       child: Row(
+        /*
+        spaceBetween : 시작과 끝에 배치하고
+        시작과 끝 위젯의 사이에 나머지 child widget을 배치한다
+        시작과 끝 사이의 위젯의 나머지 공간은 모두 균일하게 배분한다 
+        */
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Image.asset(
