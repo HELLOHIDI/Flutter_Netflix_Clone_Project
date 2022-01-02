74b94407c8e5c18993eb9ba5c5241e959fdf3e9a
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-01-02 14:24:20 +0900

    원형 슬라이더 위젯 주석 추가

--- lib/widget/circle_slider.dart
+++ lib/widget/circle_slider.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:netflixclone/model/model_movie.dart';
 import 'package:netflixclone/screen/detail_screen.dart';
 
+// 원형 영화 위젯
 class CircleSlider extends StatelessWidget {
-  final List<Movie> movies;
-  CircleSlider({required this.movies});
+  final List<Movie> movies; //영화 데이터 더미
+  CircleSlider({required this.movies}); // 영화 데이터를 홈화면으로 부터 넘겨받아야 함
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +18,7 @@ class CircleSlider extends StatelessWidget {
           Container(
             height: 120,
             child: ListView(
-              scrollDirection: Axis.horizontal,
+              scrollDirection: Axis.horizontal, //수직으로 스크롤 가능
               children: makeCircleImages(context, movies),
             ),
           ),
@@ -27,11 +28,14 @@ class CircleSlider extends StatelessWidget {
   }
 }
 
+// 원형 위젯을 만드는 함수
 List<Widget> makeCircleImages(BuildContext context, List<Movie> movies) {
   List<Widget> results = [];
+  // 리스트에 담겨있는 영화들
   for (var i = 0; i < movies.length; i++) {
     results.add(
       InkWell(
+        // 원형 영화 위젯을 누르면 더보기 스크린으로 넘어감
         onTap: () {
           Navigator.of(context).push(
             MaterialPageRoute<Null>(
@@ -44,11 +48,13 @@ List<Widget> makeCircleImages(BuildContext context, List<Movie> movies) {
             ),
           );
         },
+
+        // 원형 모양으로 제작
         child: Container(
           padding: EdgeInsets.only(right: 10),
           alignment: Alignment.centerLeft,
           child: CircleAvatar(
-            backgroundImage: AssetImage('images/' + movies[i].poster),
+            backgroundImage: NetworkImage(movies[i].poster),
             radius: 48,
           ),
         ),
