aa71449ec759f20fc4ada3c932b7157646952a34
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-01-09 19:55:48 +0900

    home_screen firebase 연동 및 주석 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,55 +3,56 @@ import 'package:netflixclone/model/model_movie.dart';
 import 'package:netflixclone/widget/carousel_slider.dart';
 import 'package:netflixclone/widget/circle_slider.dart';
 import 'package:netflixclone/widget/box_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<Movie> movies = [
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-  ];
+/*
+Query : 데이터베이스에게 특정한 데이터를 보여달라는 클라이언트의 요청
+QuerySnapshot : collection으로 부터 Query, snapshot을 통해 받아온 데이터 타입
+                사실상 Snapshot은 비동기로 실제 서버 데이터를 가져온 내용물이다.
+                Collection으로 부터 특정 Doucment들을 가져왔기에 하나씩 까봐야한다.
 
+
+
+*/
+class _HomeScreenState extends State<HomeScreen> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance; //Firestore 인스턴스 생성
+  late Stream<QuerySnapshot> streamData;
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    //Real-time Read : 실시간으로 반영됨
+    //Collection의 Stream을 받아서 전체 Documents의 변경 사항(streamData)을 실시간으로 받음
+    //'movie'는 콘솔에서 작성한 컬렉션 이름(firebase 컬렉션)
+    streamData = firestore.collection('movie').snapshots();
+  }
+
+  //streamData로부터 데이터를 추출하여 위젯생성 과정
+  Widget _fetchData(BuildContext context) {
+    //streamBuilder를 통해 streamdata를 가져옴
+    return StreamBuilder<QuerySnapshot>(
+      //snapshots() : real-time Read를 위한 Stream을 받아오는 함수
+      stream: FirebaseFirestore.instance.collection('movie').snapshots(),
+      builder: (context, snapshot) {
+        //만약 데이터가 없으면 로딩화면(//LinearProgressIndicator())
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        //데이터가 있다면 _buildBody를 호출하여 실제 위젯을 만듬
+        return _buildBody(context, snapshot.data!.docs); //docs : QuerySnapshot의 내부 데이터 리스트에 접근한다.
+      },
+    );
+  }
+
+  Widget _buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+    // movies를 선언해 snapshot으로부터 데이터를 가져옴
+    // map을 통해 데이터를 Movie모델의 형태로 바꿔주고 이를 리스트 형태
+    // [{title : dsljfa} , {like :  true} , blabla ]
+    List<Movie> movies = snapshot.map((d) => Movie.fromSnapshot(d)).toList();
     return ListView(
       children: [
+        // 각각의 위젯에 영화 더미를 넣어준다
         Stack(
           children: [
             CarouselImage(movies: movies),
@@ -62,6 +63,11 @@ class _HomeScreenState extends State<HomeScreen> {
         BoxSlider(movies: movies),
       ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _fetchData(context);
   }
 }
 
