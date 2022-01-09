a140f27d5550a86be750d9ac2bc91d7d91ba14b4
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-01-09 19:55:08 +0900

    movie modle firebase와 연동

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -1,20 +1,29 @@
-//넷플릭스 영화 모델 => 영화 데이터 관리를 쉽게 하기 위함
-  
-//모델에 들어갈 요소들을 class를 이용해 선언
-class Movie {
-  final String title; //제목
-  final String keyword; //키워드
-  final String poster; //포스터(영화 사진)
-  final bool like; //찜하기
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  //named 생성자를 이용해 개체 생성
-  Movie.fromMap(Map<String, dynamic> map)
+//넷플릭스 영화 모델
+class Movie {
+  final String title;
+  final String keyword;
+  final String poster;
+  final bool like;
+  /*DocumentReference 
+  :실제 Firebase firestore에 있는 데이터 컬럼을 참조할 수 있는 링크
+  reference를 이용해 해당 데이터에 대한 CRUD 기능을 간단히 처리 가능
+  - CRUD : Create(생성), Read(읽기), Update(갱신), Delete(삭제)
+  */
+
+  final DocumentReference reference;
+
+  Movie.fromMap(Map<String, dynamic> map, {required this.reference})
       : title = map['title'],
         keyword = map['keyword'],
         poster = map['poster'],
         like = map['like'];
   
-  //인스턴스 출력 용이를 위한 toString() 메소드 구현
+  // .data() : 실제 데이터가 들어있으며 Map<String, dynamic>형태로 넘어온다.
+  // DocumentSnapshot : 실제 Document의 데이터가 들어있는 객체이다.
+  Movie.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data() as Map<String, dynamic>, reference: snapshot.reference);
+
   @override
   String toString() => "Movie<$title:$keyword>";
 }
