04477ea56a693ad27380fbdad840cb418371948a
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2022-02-05 15:11:48 +0900

    출연자, 제작자 메소드 추가

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -2,10 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 //넷플릭스 영화 모델
 class Movie {
-  final String title;
-  final String keyword;
-  final String poster;
-  final bool like;
+  final String title; //제목
+  final String keyword; //키워드
+  final String poster; //포스터
+  final bool like; //찜하기 여부
+  final String actor; //출연자
+  final String producer; //제작자
   /*DocumentReference 
   :실제 Firebase firestore에 있는 데이터 컬럼을 참조할 수 있는 링크
   reference를 이용해 해당 데이터에 대한 CRUD 기능을 간단히 처리 가능
@@ -18,12 +20,16 @@ class Movie {
       : title = map['title'],
         keyword = map['keyword'],
         poster = map['poster'],
-        like = map['like'];
-  
-  // .data() : 실제 데이터가 들어있으며 Map<String, dynamic>형태로 넘어온다.
-  // DocumentSnapshot : 실제 Document의 데이터가 들어있는 객체이다.
+        like = map['like'],
+        actor = map['actor'],
+        producer = map['producer'];
+
   Movie.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data() as Map<String, dynamic>, reference: snapshot.reference);
 
   @override
+  //키워드 출력
   String toString() => "Movie<$title:$keyword>";
+  
+  //출연자,제작자 출력
+  String toOfficials() => "출연 : ${actor}\n제작자 : ${producer}";
 }
